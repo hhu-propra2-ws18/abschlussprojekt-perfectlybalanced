0ab4dc441b0b3678648fe1dd7aeada78c5c6cfed
Johannes <j.fusenig@gmx.net>
2019-02-12 13:29:25 +0100

    fixing models

--- src/main/java/de/hhu/abschlussprojektverleihplattform/model/LendingEntity.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/model/LendingEntity.java
@@ -12,18 +12,18 @@ public class LendingEntity {
     private Timestamp start;
     private Timestamp end;
     private UserEntity borrower;
-    private ProductEntity productEntity;
+    private ProductEntity product;
 
     public LendingEntity() {
         
     }
 
-    public LendingEntity(Lendingstatus status, Timestamp start, Timestamp end, UserEntity borrower, ProductEntity productEntity) {
+    public LendingEntity(Lendingstatus status, Timestamp start, Timestamp end, UserEntity borrower, ProductEntity product) {
         this.status = status;
         this.start = start;
         this.end = end;
         this.borrower = borrower;
-        this.productEntity = productEntity;
+        this.product = product;
     }
 
 }
--- src/main/java/de/hhu/abschlussprojektverleihplattform/model/ProductEntity.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/model/ProductEntity.java
@@ -10,16 +10,20 @@ public class ProductEntity {
     private String titel;
     private int surety;
     private int cost;
+    private AddressEntity location;
+    private UserEntity owner;
 
     public ProductEntity() {
 
     }
 
-    public ProductEntity(String description, String titel, int surety, int cost) {
+    public ProductEntity(String description, String titel, int surety, int cost, AddressEntity location, UserEntity owner) {
         this.description = description;
         this.titel = titel;
         this.surety = surety;
         this.cost = cost;
+        this.location = location;
+        this.owner = owner;
     }
     
 }
