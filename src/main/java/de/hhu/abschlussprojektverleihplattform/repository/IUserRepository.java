0f31e0caa8c83893414008c65fd07d6245b66a94
Tobias Porzelt <tobias.porzelt@hhu.de>
2019-02-13 14:01:55 +0100

    created userrepository and tested startconfig of database

---
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/repository/IUserRepository.java
@@ -0,0 +1,14 @@
+package de.hhu.abschlussprojektverleihplattform.repository;
+
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+
+import java.util.List;
+
+public interface IUserRepository {
+
+    UserEntity findById(Long userId);
+    void saveUser(UserEntity user);
+    int getNumberOfUsers();
+    List<UserEntity> alleUser();
+
+}
