f3d855424bd5babb180293386ab92e6b439aa6fb
Tobias Porzelt <tobias.porzelt@hhu.de>
2019-02-13 14:15:57 +0100

    merged Develope-Branch, optimized Tests and added datasource attribute

--- src/test/java/de/hhu/abschlussprojektverleihplattform/repository/UserRepositoryTest.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/repository/UserRepositoryTest.java
@@ -1,21 +1,12 @@
 package de.hhu.abschlussprojektverleihplattform.repository;
 
-import de.hhu.abschlussprojektverleihplattform.AbschlussprojektVerleihplattformApplication;
 import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
-import org.apache.coyote.http11.Constants;
-import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.xml.ws.WebEndpoint;
 
 
 @RunWith(SpringRunner.class)
@@ -30,7 +21,7 @@ public class UserRepositoryTest {
     public void startConfigTestOneUseerIsInDatabase(){
 
         int numberOfUsers = userRepository.getNumberOfUsers();
-        Assert.assertTrue(numberOfUsers == 1);
+        Assert.assertEquals(1, numberOfUsers);
 
     }
 
@@ -40,7 +31,7 @@ public class UserRepositoryTest {
         UserEntity user = new UserEntity("firstName", "LastName", "username", "password", "email");
         userRepository.saveUser(user);
         int numberOfUsers = userRepository.getNumberOfUsers();
-        Assert.assertTrue(numberOfUsers == 2);
+        Assert.assertEquals(2, numberOfUsers);
 
     }
 
@@ -48,7 +39,7 @@ public class UserRepositoryTest {
     public void startConfigTestLoadMaxMusterMann(){
 
         UserEntity user = new UserEntity("Max", "Mustermann", "MMustermann", "MaxMuster223", "Max@Mustermann.de");
-        UserEntity loadedUser = userRepository.findById(new Long(1));
+        UserEntity loadedUser = userRepository.findById(1L);
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + loadedUser);
         Assert.assertTrue(user.getFirstname().equals(loadedUser.getFirstname()) &&
                                     user.getLastname().equals(loadedUser.getLastname()) &&
