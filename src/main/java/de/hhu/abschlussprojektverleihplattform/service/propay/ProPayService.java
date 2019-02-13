30453eba8af23a7140e9da978687f1691d02ddcd
pointbazaar <alex23667@gmail.com>
2019-02-13 11:43:07 +0100

    minor changes

--- src/main/java/de/hhu/abschlussprojektverleihplattform/service/propay/ProPayService.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/service/propay/ProPayService.java
@@ -1,6 +1,7 @@
 package de.hhu.abschlussprojektverleihplattform.service.propay;
 
 import de.hhu.abschlussprojektverleihplattform.service.propay.model.Account;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 public class ProPayService implements IProPayService{
@@ -29,6 +30,27 @@ public class ProPayService implements IProPayService{
         Account account = restTemplate.getForObject(url,Account.class);
 
         return account.amount;
+    }
+
+    @Override
+    public boolean accountExists(String username) {
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            String url = baseurl + "account/" + username;
+            Account account = restTemplate.getForObject(url, Account.class);
+        }catch (HttpClientErrorException ex){
+            if(ex.getStatusCode().is4xxClientError()){
+
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean makePayment(String sourceAccount, String targetAccount, long amount) {
+        return false;
     }
     //TODO: implement service that makes the requests
 }
