5c9ecc409751f6e9ea0753352a3a9b73cfbf1d8b
Huyen Chantal Nguyen <chantalnguyen@hotmail.de>
2019-02-13 12:18:15 +0100

    add application test

--- src/test/java/de/hhu/abschlussprojektverleihplattform/AbschlussprojektVerleihplattformApplicationTests.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/AbschlussprojektVerleihplattformApplicationTests.java
@@ -1,16 +1,16 @@
-//package de.hhu.abschlussprojektverleihplattform;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class AbschlussprojektVerleihplattformApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package de.hhu.abschlussprojektverleihplattform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbschlussprojektVerleihplattformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
