fba720690e7a5b3bb4535f07070b8a31005816a5
Johannes <j.fusenig@gmx.net>
2019-02-13 13:18:35 +0100

    add a feedback to generate a success/fail-message on the view

--- src/main/java/de/hhu/abschlussprojektverleihplattform/logic/IPayment.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/logic/IPayment.java
@@ -8,9 +8,9 @@ public interface IPayment {
     // kann ggf auch void sein, haengt davon ab ob eine bestaetigung notwendig ist oder nicht
     boolean reservateAmount(UserEntity user, int amount);
 
-    void tranferReservatedMoney(UserEntity payingUser, UserEntity recivingUser, int amount);
+    boolean tranferReservatedMoney(UserEntity payingUser, UserEntity recivingUser, int amount);
 
-    void returnReservatedMoney(UserEntity userEntity, int amount);
+    boolean returnReservatedMoney(UserEntity userEntity, int amount);
 
 
     // Da ich die genaue funktionsweise des reservierens nicht kenne, kann sich hier noch einiges aendern
