fb7fff546048ae476fd76b05cee5f0383db28cd3
pointbazaar <alex23667@gmail.com>
2019-02-13 11:07:29 +0100

    make an interface for the propay service

---
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/service/IProPayService.java
@@ -0,0 +1,6 @@
+package de.hhu.abschlussprojektverleihplattform.service;
+
+public interface IProPayService {
+
+    public int checkbalance(String username);
+}
---
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/service/ProPayService.java
@@ -0,0 +1,5 @@
+package de.hhu.abschlussprojektverleihplattform.service;
+
+public class ProPayService {
+    //TODO: implement service that makes the requests
+}
