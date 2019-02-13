96bae8e663822f04f2e901fa3a59a5c126594a4b
pointbazaar <alex23667@gmail.com>
2019-02-13 10:38:32 +0100

    jdbc template rowcallbackhandler

--- src/main/java/de/hhu/abschlussprojektverleihplattform/repository/ProductRepository.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/repository/ProductRepository.java
@@ -1,14 +1,36 @@
 package de.hhu.abschlussprojektverleihplattform.repository;
 
 import de.hhu.abschlussprojektverleihplattform.model.ProductEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepository implements IProductRepository {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public List<ProductEntity> findAll() {
-        return null;
+        ArrayList<ProductEntity> products=new ArrayList<>();
+
+        jdbcTemplate.query("select * from product",
+            new RowCallbackHandler() {
+                @Override
+                public void processRow(ResultSet resultSet) throws SQLException {
+                    while(resultSet.next()) {
+
+                        products.add(null);
+                    }
+                }
+            }
+        );
+        return products;
     }
 
     @Override
