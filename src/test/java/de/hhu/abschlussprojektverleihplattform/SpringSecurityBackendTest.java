f4144f7f7a400b802bdb47c641336197c0ebeaf9
Liane Berger <liane-berger@outlook.de>
2019-02-13 11:20:11 +0100

    add test - security works

--- src/test/java/de/hhu/abschlussprojektverleihplattform/SpringSecurityBackendTest.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/SpringSecurityBackendTest.java
@@ -1,15 +1,41 @@
 package de.hhu.abschlussprojektverleihplattform;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
+@AutoConfigureMockMvc
 public class SpringSecurityBackendTest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void contextLoads() {
     }
+
+    @Test
+    @WithMockUser
+    public void testSecurityWorks() throws Exception {
+        mvc.perform(
+                get("/login"))
+                .andExpect(content().string(containsString("Verleihplattform")))
+                .andExpect(content().string(containsString("Logout")))
+                .andExpect(content().string(Matchers.containsString("Produkte ansehen")));
+
+    }
+
 }
