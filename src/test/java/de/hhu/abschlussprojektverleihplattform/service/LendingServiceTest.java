cd8e128783f93a40a19ba1c15a5f35ef16db6852
Johannes <j.fusenig@gmx.net>
2019-02-14 10:34:59 +0100

    Prepare for Tests

--- src/test/java/de/hhu/abschlussprojektverleihplattform/service/LendingServiceTest.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/service/LendingServiceTest.java
@@ -1,43 +1,80 @@
 package de.hhu.abschlussprojektverleihplattform.service;
 
+import de.hhu.abschlussprojektverleihplattform.model.AddressEntity;
+import de.hhu.abschlussprojektverleihplattform.model.ProductEntity;
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import org.h2.engine.User;
+
 public class LendingServiceTest {
-    private String firstname1 = "Frank";
-    private String lastname1 = "Meier";
-    private String username1 = "DerTolleFrank";
-    private String password1 = "123456";
-    private String email1 = "Frank.Meier@Example.com";
-    private String description1 = "Ein toller Rasemäher";
-    private String title1 = "Rasemäher";
-    private int surety1 = 200;
-    private int cost1 = 20;
-    private String street1 = "Tulpenweg";
-    private int housenumber1 = 33;
-    private int postcode1 = 12345;
-    private String city1 = "Heidelberg";
-    private String firstname2 = "Hans";
-    private String lastname2 = "Müller";
-    private String username2 = "Hanswurst";
-    private String password2 = "654321";
-    private String email2 = "Hans1993@Example.com";
-    private String description2 = "Eine Heckenschere";
-    private String title2 = "Heckenschere";
-    private int surety2 = 60;
-    private int cost2 = 5;
-    private String street2 = "Talstrasse";
-    private int housenumber2 = 44;
-    private int postcode2 = 67890;
-    private String city2 = "Bad Salzburg";
-    private String firstname3 = "Dieter";
-    private String lastname3 = "Schulz";
-    private String username3 = "VollCoolerName";
-    private String password3 = "qwertz";
-    private String email3 = "D.Schulz@Example.com";
-    private String description3 = "Ein richtig fetter Kohle-Grill";
-    private String title3 = "Grill";
-    private int surety3 = 3000;
-    private int cost3 = 800;
-    private String street3 = "Lilienpfad";
-    private int housenumber3 = 6;
-    private int postcode3 = 13579;
-    private String city3 = "Niederhausen";
+
+
+
+    // private Methoden um schnell an TestEntities zu kommen
+
+    private UserEntity createExampleUser1() {
+        String firstname = "Frank";
+        String lastname = "Meier";
+        String username = "DerTolleFrank";
+        String password = "123456";
+        String email = "Frank.Meier@Example.com";
+        return new UserEntity(firstname, lastname, username, password, email);
+    }
+
+    private UserEntity createExampleUser2() {
+        String firstname = "Hans";
+        String lastname = "Müller";
+        String username = "Hnaswurst";
+        String password = "qwertz";
+        String email = "D.Schulz@Example.com";
+        return new UserEntity(firstname, lastname, username, password, email);
+    }
+
+    private UserEntity createExampleUser3() {
+        String firstname = "Dieter";
+        String lastname = "Schulz";
+        String username = "VollCoolerName";
+        String password = "123456";
+        String email = "Frank.Meier@Example.com";
+        return new UserEntity(firstname, lastname, username, password, email);
+    }
+
+    private ProductEntity createExampleProduct1(UserEntity owner) {
+        String description = "Ein toller Rasemäher";
+        String title = "Rasemäher";
+        int surety = 200;
+        int cost = 20;
+        String street = "Tulpenweg";
+        int housenumber = 33;
+        int postcode = 12345;
+        String city = "Heidelberg";
+        AddressEntity a = new AddressEntity(street, housenumber, postcode, city);
+        return new ProductEntity(description, title, surety, cost, a, owner);
+    }
+
+    private ProductEntity createExampleProduct2(UserEntity owner) {
+        String description = "Eine Heckenschere";
+        String title = "Heckenschere";
+        int surety = 60;
+        int cost = 5;
+        String street = "Talstrasse";
+        int housenumber = 44;
+        int postcode = 67890;
+        String city = "Bad Salzbug";
+        AddressEntity a = new AddressEntity(street, housenumber, postcode, city);
+        return new ProductEntity(description, title, surety, cost, a, owner);
+    }
+
+    private ProductEntity createExampleProduct3(UserEntity owner) {
+        String description = "Ein richtig fetter Kohle-Grill";
+        String title = "Grill";
+        int surety = 3000;
+        int cost = 800;
+        String street = "Lilienpfad";
+        int housenumber = 6;
+        int postcode = 13579;
+        String city = "Niederhausen";
+        AddressEntity a = new AddressEntity(street, housenumber, postcode, city);
+        return new ProductEntity(description, title, surety, cost, a, owner);
+    }
+
 }
