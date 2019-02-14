0a312fc0bdcefc9f542d5d17d0e37e53718eea90
Johannes <j.fusenig@gmx.net>
2019-02-14 10:16:20 +0100

    Adapt TestClass

---
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/service/LendingServiceTest.java
@@ -0,0 +1,43 @@
+package de.hhu.abschlussprojektverleihplattform.service;
+
+public class LendingServiceTest {
+    private String firstname1 = "Frank";
+    private String lastname1 = "Meier";
+    private String username1 = "DerTolleFrank";
+    private String password1 = "123456";
+    private String email1 = "Frank.Meier@Example.com";
+    private String description1 = "Ein toller Rasemäher";
+    private String title1 = "Rasemäher";
+    private int surety1 = 200;
+    private int cost1 = 20;
+    private String street1 = "Tulpenweg";
+    private int housenumber1 = 33;
+    private int postcode1 = 12345;
+    private String city1 = "Heidelberg";
+    private String firstname2 = "Hans";
+    private String lastname2 = "Müller";
+    private String username2 = "Hanswurst";
+    private String password2 = "654321";
+    private String email2 = "Hans1993@Example.com";
+    private String description2 = "Eine Heckenschere";
+    private String title2 = "Heckenschere";
+    private int surety2 = 60;
+    private int cost2 = 5;
+    private String street2 = "Talstrasse";
+    private int housenumber2 = 44;
+    private int postcode2 = 67890;
+    private String city2 = "Bad Salzburg";
+    private String firstname3 = "Dieter";
+    private String lastname3 = "Schulz";
+    private String username3 = "VollCoolerName";
+    private String password3 = "qwertz";
+    private String email3 = "D.Schulz@Example.com";
+    private String description3 = "Ein richtig fetter Kohle-Grill";
+    private String title3 = "Grill";
+    private int surety3 = 3000;
+    private int cost3 = 800;
+    private String street3 = "Lilienpfad";
+    private int housenumber3 = 6;
+    private int postcode3 = 13579;
+    private String city3 = "Niederhausen";
+}
