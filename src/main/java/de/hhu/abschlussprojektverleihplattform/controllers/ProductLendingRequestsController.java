3ca4a4481ca515cb4a51946b7985e595b6fa2d5a
Jan Rahman <jan.rahman@hhu.de>
2019-02-14 09:27:22 +0100

    Annotation hinzugefügt

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductLendingRequestsController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductLendingRequestsController.java
@@ -2,13 +2,14 @@ package de.hhu.abschlussprojektverleihplattform.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductLendingRequestsController {
 
     @GetMapping("/lendingrequests")
-    public String getLendingRequestsOverview(){
+    public String getLendingRequestsOverview(Model model){
         //TODO: determine logged in user,
         //otherwise redirect to login page
 
