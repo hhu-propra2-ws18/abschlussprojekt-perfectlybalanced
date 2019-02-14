d1b33ac6a19212b76d252236dae41f5689519d98
Jan Rahman <jan.rahman@hhu.de>
2019-02-14 13:59:05 +0100

    User Service, IUserService = IUser

--- src/main/java/de/hhu/abschlussprojektverleihplattform/service/UserService.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/service/UserService.java
@@ -1,15 +1,26 @@
 package de.hhu.abschlussprojektverleihplattform.service;
 
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import de.hhu.abschlussprojektverleihplattform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService {
+public class UserService implements IUserService {
 
     @Autowired
-    private JdbcTemplate jtm;
-    
+    private UserRepository userRepository;
+
+    @Override
+    public void addUser(UserEntity user) {
+        userRepository.saveUser(user);
+    }
+
+    @Override
+    public UserEntity showUserById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
 }
