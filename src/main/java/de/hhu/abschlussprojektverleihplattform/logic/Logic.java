e133d9e198a505963542acbf5ce4ecec01f070f9
Johannes <j.fusenig@gmx.net>
2019-02-14 10:04:24 +0100

    Adapt Logic to new IPayment

--- src/main/java/de/hhu/abschlussprojektverleihplattform/logic/Logic.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/logic/Logic.java
@@ -13,30 +13,28 @@ public class Logic {
 
     private ILending lending_service;
     private IPayment payment_service;
-    private IProduct product_service;
-    private IUser user_service;
 
-    public Logic(ILending lending_service, IPayment payment_service, IProduct product_service, IUser user_service) {
+    public Logic(ILending lending_service, IPayment payment_service) {
         this.lending_service = lending_service;
         this.payment_service = payment_service;
-        this.product_service = product_service;
-        this.user_service = user_service;
     }
 
     // werden momentan nicht gebraucht
     //private IAddress address_service;
     //private IAdmin admin_service;
+    //private IProduct product_service;
+    //private IUser user_service;
 
-    public void AddUser(String firstname, String lastname, String username, String password, String email) {
-        UserEntity u = new UserEntity(firstname, lastname, username, password, email);
-        user_service.addUser(u);
-    }
+//    public void AddUser(String firstname, String lastname, String username, String password, String email) {
+//        UserEntity u = new UserEntity(firstname, lastname, username, password, email);
+//        user_service.addUser(u);
+//    }
 
-    public void AddProduct(UserEntity actingUser, String description, String titel, int surety, int cost, String street, int housenumber, int postcode, String city) {
-        AddressEntity location = new AddressEntity(street, housenumber, postcode, city);
-        ProductEntity p = new ProductEntity(description, titel, surety, cost, location, actingUser);
-        product_service.addProduct(p);
-    }
+//    public void AddProduct(UserEntity actingUser, String description, String titel, int surety, int cost, String street, int housenumber, int postcode, String city) {
+//        AddressEntity location = new AddressEntity(street, housenumber, postcode, city);
+//        ProductEntity p = new ProductEntity(description, titel, surety, cost, location, actingUser);
+//        product_service.addProduct(p);
+//    }
 
     //// Operationen:
 
@@ -61,26 +59,25 @@ public class Logic {
         int totalcost = product.getCost() + product.getSurety();
         boolean MoneyIsOK = payment_service.UserHasAmount(actingUser, totalcost);
         if (TimeIsOK && MoneyIsOK) {
-//            LendingEntity lending = new LendingEntity(Lendingstatus.requested, start, end, actingUser, product);
-//            lending_service.addLending(lending);
-//            if (payment_service.reservateAmount(actingUser, product.getOwner(), product.getSurety())) {
-//                if(payment_service.reservateAmount(actingUser, product.getOwner(), product.getCost())) {
-//                    return true;
-//                }
-//                payment_service.returnReservatedMoney(actingUser, product.getSurety());
-//            }
+            Long costID = payment_service.reservateAmount(actingUser, product.getOwner(), product.getCost());
+            Long suretyID = payment_service.reservateAmount(actingUser, product.getOwner(), product.getSurety());
+            if(costID > 0 && suretyID > 0) {
+                LendingEntity lending = new LendingEntity(Lendingstatus.requested, start, end, actingUser, product, costID, suretyID);
+                lending_service.addLending(lending);
+                return true;
+            } else {
+                payment_service.returnReservatedMoney(costID);
+                payment_service.returnReservatedMoney(suretyID);
+            }
         }
         return false;
     }
 
     // Anfrage einer Buchung beantworten
-    public boolean AcceptLending(UserEntity actingUser, LendingEntity lending) {
+    public boolean AcceptLending(LendingEntity lending) {
         lending.setStatus(Lendingstatus.confirmt);
         lending_service.update(lending);
-        UserEntity productowner = lending.getProduct().getOwner();
-        int cost = lending.getProduct().getCost();
-//        return payment_service.tranferReservatedMoney(actingUser, productowner, cost);
-        return false;
+        return payment_service.tranferReservatedMoney(lending.getCostReservationID());
     }
 
     // Artikel zurueckgeben
@@ -101,10 +98,7 @@ public class Logic {
         if (isAcceptable) {
             lending.setStatus(Lendingstatus.done);
             lending_service.update(lending);
-            UserEntity customer = lending.getBorrower();
-            int surety = lending.getProduct().getSurety();
-//            return payment_service.returnReservatedMoney(customer, surety);
-            return false;
+            return payment_service.returnReservatedMoney(lending.getSuretyReservationID());
         } else {
             lending.setStatus(Lendingstatus.conflict);
             return true;
@@ -117,10 +111,7 @@ public class Logic {
         if (isAcceptable) {
             lending.setStatus(Lendingstatus.done);
             lending_service.update(lending);
-            UserEntity customer = lending.getBorrower();
-            int surety = lending.getProduct().getSurety();
-//            return payment_service.returnReservatedMoney(customer, surety);
-            return false;
+            return payment_service.returnReservatedMoney(lending.getSuretyReservationID());
         } else {
             lending.setStatus(Lendingstatus.conflict);
             return true;
@@ -130,18 +121,13 @@ public class Logic {
     // Konflikt vom Admin loesen
     public boolean ResolveConflict(LendingEntity lending, boolean OwnerRecivesSurety) {
         if (OwnerRecivesSurety) {
-            UserEntity customer = lending.getBorrower();
-            UserEntity owner = lending.getProduct().getOwner();
-            int surety = lending.getProduct().getSurety();
-//            if (!payment_service.tranferReservatedMoney(customer, owner, surety)) {
-//                return false;
-//            }
+            if(!payment_service.tranferReservatedMoney(lending.getSuretyReservationID())) {
+                return false;
+            }
         } else {
-            UserEntity customer = lending.getBorrower();
-            int surety = lending.getProduct().getSurety();
-//            if (!payment_service.reservateAmount(customer, surety)) {
-//                return false;
-//            }
+            if(!payment_service.tranferReservatedMoney(lending.getSuretyReservationID())) {
+                return false;
+            }
         }
         lending.setStatus(Lendingstatus.done);
         lending_service.update(lending);
@@ -151,33 +137,33 @@ public class Logic {
     //// Abfragen:
 
     // Alle Produkte
-    public List<ProductEntity> GetAllProducts() {
-        return product_service.getAllProducts();
-    }
+//    public List<ProductEntity> GetAllProducts() {
+//        return product_service.getAllProducts();
+//    }
 
     // Alle eingehenden Anfragen
-    public List<LendingEntity> GetRequestForUser(UserEntity actingUser) {
-        return lending_service.getAllRequestsForUser(actingUser);
-    }
+//    public List<LendingEntity> GetRequestForUser(UserEntity actingUser) {
+//        return lending_service.getAllRequestsForUser(actingUser);
+//    }
 
     // Alle geliehenen Produkte
-    public List<LendingEntity> GetLendingForUser(UserEntity actingUser) {
-        return lending_service.getAllLendingsForUser(actingUser);
-    }
+//    public List<LendingEntity> GetLendingForUser(UserEntity actingUser) {
+//        return lending_service.getAllLendingsForUser(actingUser);
+//    }
 
     // Alle verliehenden Produkte
-    public List<LendingEntity> GetLedingsFromUser(UserEntity actingUser) {
-        return lending_service.getAllLendingsFromUser(actingUser);
-    }
+//    public List<LendingEntity> GetLedingsFromUser(UserEntity actingUser) {
+//        return lending_service.getAllLendingsFromUser(actingUser);
+//    }
 
     // Alle zurueckgegebene Produkte
-    public List<LendingEntity> GetReturnedLendings(UserEntity activeUser) {
-        return lending_service.getReturnedLendingFromUser(activeUser);
-    }
+//    public List<LendingEntity> GetReturnedLendings(UserEntity activeUser) {
+//        return lending_service.getReturnedLendingFromUser(activeUser);
+//    }
 
     // Alle Konflikte
-    public List<LendingEntity> GetAllConflicts() {
-        return lending_service.getAllConflicts();
-    }
+//    public List<LendingEntity> GetAllConflicts() {
+//        return lending_service.getAllConflicts();
+//    }
 
 }
