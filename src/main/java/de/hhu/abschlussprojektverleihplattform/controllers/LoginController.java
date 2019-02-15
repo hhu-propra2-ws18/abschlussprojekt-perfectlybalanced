a5714c1e3657172198b24705e5aebb695e2a898b
Jan Rahman <jan.rahman@hhu.de>
2019-02-15 12:49:30 +0100

    find By Username Repo

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/LoginController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/LoginController.java
@@ -1,20 +1,38 @@
 package de.hhu.abschlussprojektverleihplattform.controllers;
 
+import de.hhu.abschlussprojektverleihplattform.repository.UserRepository;
+import de.hhu.abschlussprojektverleihplattform.service.CookieUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController {
 
+    @Autowired
+    CookieUserService cookieUserService;
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/login")
-    public String getLogin(){
-        return "login";
+    public String getLogin(HttpServletRequest httpServletRequest){
+        try {
+            cookieUserService.getUserFromRequest(httpServletRequest);
+        }catch (Exception e){
+            return "login";
+        }
+        return "redirect:/";
     }
 
     @PostMapping("/login")
-    public String postLogin(@RequestParam(value="username")String username, @RequestParam(value="password")String password){
+    public String postLogin(@RequestParam(value="username")String username,
+                            @RequestParam(value="password")String password,
+                            HttpServletRequest httpServletRequest){
         //TODO: actually check user credentials submitted
         return "redirect:/profile";
     }
--- src/main/java/de/hhu/abschlussprojektverleihplattform/repository/IUserRepository.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/repository/IUserRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface IUserRepository {
 
     UserEntity findById(Long userId);
+    UserEntity findByUsername(String username);
     void saveUser(UserEntity user);
     int getNumberOfUsers();
     List<UserEntity> getAllUser();
