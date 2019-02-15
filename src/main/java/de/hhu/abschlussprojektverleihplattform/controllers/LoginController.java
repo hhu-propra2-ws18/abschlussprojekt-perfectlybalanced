48c7f6f7167136ddb300fbc62bdcf39124217200
Jan Rahman <jan.rahman@hhu.de>
2019-02-15 12:57:59 +0100

    Cookie-Config in LoginController

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/LoginController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/LoginController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -18,6 +20,9 @@ public class LoginController {
 
     @Autowired
     UserRepository userRepository;
+
+
+
 
     @GetMapping("/login")
     public String getLogin(HttpServletRequest httpServletRequest){
@@ -32,8 +37,20 @@ public class LoginController {
     @PostMapping("/login")
     public String postLogin(@RequestParam(value="username")String username,
                             @RequestParam(value="password")String password,
-                            HttpServletRequest httpServletRequest){
+                            HttpServletResponse response){
         //TODO: actually check user credentials submitted
+        try {
+            userRepository.findByUsername(username).getUsername();
+        } catch (Exception e) {
+            return "login";
+        }
+        response.addCookie(
+                new Cookie(
+                        CookieUserService.cookieName,
+                        userRepository.findByUsername(username).getUserId()+""
+                )
+        );
+
         return "redirect:/profile";
     }
 
