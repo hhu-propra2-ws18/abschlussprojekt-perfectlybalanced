3d875742abea2f6bc2c8bd31fc1f1d5bd691f059
Jan Rahman <jan.rahman@hhu.de>
2019-02-15 13:55:52 +0100

    UserProfile mit Cookies

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductController.java
@@ -1,32 +1,76 @@
 package de.hhu.abschlussprojektverleihplattform.controllers;
 
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import de.hhu.abschlussprojektverleihplattform.repository.UserRepository;
+import de.hhu.abschlussprojektverleihplattform.service.CookieUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProductController {
 
     // PostMapping fehlt noch
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CookieUserService cookieUserService;
+
     @GetMapping("/addproduct")
-    public String getAddProduct(Model model) {
+    public String getAddProduct(Model model, HttpServletRequest httpServletRequest) {
+        UserEntity user;
+
+        try {
+            user = cookieUserService.getUserFromRequest(httpServletRequest);
+        }catch (Exception e){
+            return "login";
+        }
+
         return "addproduct";
     }
 
     @GetMapping("/editproduct")
-    public String getEditProduct(Model model) {
-        // Was noch fehlt: Einträge vom Produkt in den Formularfeldern anzeigen lassen
+    public String getEditProduct(Model model, HttpServletRequest httpServletRequest) {
+
+        UserEntity user;
+
+        try {
+            cookieUserService.getUserFromRequest(httpServletRequest);
+        }catch (Exception e){
+            return "login";
+        }
+
         return "editproduct";
     }
 
     @GetMapping("/removeproduct")
-    public String getRemoveProduct(Model model) {
+    public String getRemoveProduct(Model model, HttpServletRequest httpServletRequest) {
+        UserEntity user;
+
+        try {
+            cookieUserService.getUserFromRequest(httpServletRequest);
+        }catch (Exception e){
+            return "login";
+        }
+
         return "removeproduct";
     }
 
     @GetMapping("/productdetail")
-    public String getProductDetails(Model model) {
+    public String getProductDetails(Model model, HttpServletRequest httpServletRequest) {
+        UserEntity user;
+
+        try {
+            cookieUserService.getUserFromRequest(httpServletRequest);
+        }catch (Exception e){
+            return "login";
+        }
+
         return "productdetailedview";
     }
 }
--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductLendingRequestsController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductLendingRequestsController.java
@@ -1,17 +1,38 @@
 package de.hhu.abschlussprojektverleihplattform.controllers;
 
 
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import de.hhu.abschlussprojektverleihplattform.repository.UserRepository;
+import de.hhu.abschlussprojektverleihplattform.service.CookieUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ProductLendingRequestsController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CookieUserService cookieUserService;
+
     @GetMapping("/lendingrequests")
-    public String getLendingRequestsOverview(Model model){
+    public String getLendingRequestsOverview(Model model, HttpServletRequest httpServletRequest){
         //TODO: determine logged in user,
         //otherwise redirect to login page
+
+        UserEntity user;
+
+        try {
+            cookieUserService.getUserFromRequest(httpServletRequest);
+        }catch (Exception e){
+            return "login";
+        }
+
 
         return "productlendingrequests";
     }
