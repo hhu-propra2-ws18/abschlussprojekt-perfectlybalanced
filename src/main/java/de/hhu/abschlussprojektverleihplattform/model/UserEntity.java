7f931647ac25a2f8308da1bc89e5bd62da8d821f
Liane Berger <liane-berger@outlook.de>
2019-02-18 11:39:45 +0100

    imports gefixt

--- src/main/java/de/hhu/abschlussprojektverleihplattform/model/UserEntity.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/model/UserEntity.java
@@ -2,10 +2,7 @@ package de.hhu.abschlussprojektverleihplattform.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
