799823237c7c2ae8ceb0f76f0ad3b731554108b3
Liane Berger <liane-berger@outlook.de>
2019-02-18 12:20:03 +0100

    delete unused cookieservice

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductController.java
@@ -1,8 +1,9 @@
 package de.hhu.abschlussprojektverleihplattform.controllers;
 
+import de.hhu.abschlussprojektverleihplattform.model.ProductEntity;
 import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import de.hhu.abschlussprojektverleihplattform.repository.ProductRepository;
 import de.hhu.abschlussprojektverleihplattform.repository.UserRepository;
-import de.hhu.abschlussprojektverleihplattform.service.CookieUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,59 +22,24 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @Autowired
-    CookieUserService cookieUserService;
 
     @GetMapping("/addproduct")
     public String getAddProduct(Model model, HttpServletRequest httpServletRequest) {
-        UserEntity user;
-
-        try {
-            user = cookieUserService.getUserFromRequest(httpServletRequest);
-        }catch (Exception e){
-            return "login";
-        }
-
         return "addproduct";
     }
 
     @GetMapping("/editproduct")
     public String getEditProduct(Model model, HttpServletRequest httpServletRequest) {
-
-        UserEntity user;
-
-        try {
-            cookieUserService.getUserFromRequest(httpServletRequest);
-        }catch (Exception e){
-            return "login";
-        }
-
         return "editproduct";
     }
 
     @GetMapping("/removeproduct")
     public String getRemoveProduct(Model model, HttpServletRequest httpServletRequest) {
-        UserEntity user;
-
-        try {
-            cookieUserService.getUserFromRequest(httpServletRequest);
-        }catch (Exception e){
-            return "login";
-        }
-
         return "removeproduct";
     }
 
     @GetMapping("/productdetail/{id}")
     public String getProductDetails(Model model, Long id, HttpServletRequest httpServletRequest) {
-        UserEntity user;
-
-        try {
-            cookieUserService.getUserFromRequest(httpServletRequest);
-        }catch (Exception e){
-            return "login";
-        }
-
         ProductEntity product = productRepository.getProductById(id);
         model.addAttribute("product", product);
         return "productdetailedview";
