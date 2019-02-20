6caec6fec434f662431db5029319b0199fe4e537
Tobias Porzelt <tobias.porzelt@hhu.de>
2019-02-20 11:48:51 +0100

    fixed test

--- src/test/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductLendingRequestsControllerTest.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/controllers/ProductLendingRequestsControllerTest.java
@@ -8,26 +8,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductLendingRequestsControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Mock
