9f59306ef49b63f1bf5b13f5f073a632b9a9704f
Johannes <j.fusenig@gmx.net>
2019-02-20 13:51:30 +0100

    add a dummy ProPay for view-development

--- src/main/java/de/hhu/abschlussprojektverleihplattform/service/LendingService.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/service/LendingService.java
@@ -4,6 +4,7 @@ import de.hhu.abschlussprojektverleihplattform.repository.ILendingRepository;
 import de.hhu.abschlussprojektverleihplattform.service.propay.IPaymentService;
 import de.hhu.abschlussprojektverleihplattform.logic.TempZeitraumModel;
 import de.hhu.abschlussprojektverleihplattform.model.*;
+import de.hhu.abschlussprojektverleihplattform.testdummys.PaymentServiceDummy;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -19,6 +20,7 @@ public class LendingService implements ILendingService {
 
     //For the development of the Controllers/Views
     private static boolean ReturnExampleLendings = true;
+    private static boolean UseDummyProPay = true;
 
     private ILendingRepository lending_repository;
     private IPaymentService payment_service;
@@ -26,6 +28,9 @@ public class LendingService implements ILendingService {
     public LendingService(ILendingRepository lending_repository, IPaymentService payment_service) {
         this.lending_repository = lending_repository;
         this.payment_service = payment_service;
+        if (UseDummyProPay) {
+            this.payment_service = new PaymentServiceDummy(true, true, true, true);
+        }
     }
 
     // Verfuegbaren Zeitraum pruefen
