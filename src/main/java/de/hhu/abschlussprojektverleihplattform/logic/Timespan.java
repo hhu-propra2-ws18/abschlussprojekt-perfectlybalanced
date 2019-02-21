d6ef41641acd241915e17c90be2e8bb6a8662a51
Johannes <j.fusenig@gmx.net>
2019-02-21 13:59:07 +0100

    refactor

--- src/main/java/de/hhu/abschlussprojektverleihplattform/logic/Timespan.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/logic/Timespan.java
@@ -1,22 +1,14 @@
 package de.hhu.abschlussprojektverleihplattform.logic;
 
 import lombok.Data;
-
-import javax.persistence.Entity;
-import java.sql.Date;
 import java.sql.Timestamp;
 
 @Data
-//@Entity
 public class Zeitspanne {
 
- //   private Date start;
- //   private Date end;
     private Timestamp start, end;
 
     public Zeitspanne(Timestamp start, Timestamp end) {
-        //this.start = new Date(start.getTime());
-        //this.end = new Date(end.getTime());
         this.start = start;
         this.end = end;
     }
