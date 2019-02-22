dbc675f19c002fc7174ab2021b9ec6e22f29d57e
Sam <sam@rassaei.com>
2019-02-22 13:56:41 +0100

    add annotations

--- src/main/java/de/hhu/abschlussprojektverleihplattform/logic/Timespan.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/logic/Timespan.java
@@ -1,12 +1,15 @@
 package de.hhu.abschlussprojektverleihplattform.logic;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.Data;
 import java.sql.Timestamp;
 
 @Data
 public class Timespan {
 
+    @SuppressFBWarnings(justification="generated code")
     private Timestamp start;
+    @SuppressFBWarnings(justification="generated code")
     private Timestamp end;
 
     public Timespan(Timestamp start, Timestamp end) {
