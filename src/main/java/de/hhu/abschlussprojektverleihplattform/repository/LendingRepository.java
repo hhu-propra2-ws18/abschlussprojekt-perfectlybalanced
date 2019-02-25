dfe96fd39aa43dfdf18261114a27d05e4b2dbbe2
Sam <sam@rassaei.com>
2019-02-25 12:53:28 +0100

    reinsert removed import

--- src/main/java/de/hhu/abschlussprojektverleihplattform/repository/LendingRepository.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/repository/LendingRepository.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Objects;
 
 import static de.hhu.abschlussprojektverleihplattform.database.DBUtils.psc;
 
