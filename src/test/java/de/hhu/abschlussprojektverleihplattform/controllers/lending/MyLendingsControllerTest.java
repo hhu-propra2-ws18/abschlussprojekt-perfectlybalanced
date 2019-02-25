1f8913315f3e185788c1b6626786608c003f6dbb
Huyen Chantal Nguyen <chantalnguyen@hotmail.de>
2019-02-25 15:10:49 +0100

    revert changes

--- src/test/java/de/hhu/abschlussprojektverleihplattform/controllers/lending/MyLendingsControllerTest.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/controllers/lending/MyLendingsControllerTest.java
@@ -11,18 +11,15 @@ import de.hhu.abschlussprojektverleihplattform.utils.RandomTestData;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.sql.Timestamp;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -38,13 +35,13 @@ public class MyLendingsControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     UserService userService;
 
     @Autowired
     AuthenticatedUserService authenticatedUserService;
 
-    @MockBean
+    @Autowired
     ProductService productService;
 
     @Autowired
@@ -60,54 +57,48 @@ public class MyLendingsControllerTest {
 
     @Test
     public void testCanSeeMyLendingRequest() throws Exception{
-        UserEntity user_owner = RandomTestData.newRandomTestUser();
-        user_owner.setUserId(5L);
-        UserEntity user_wannabe_borrower = RandomTestData.newRandomTestUser();
-        user_wannabe_borrower.setUserId(2L);
-        ProductEntity productEntity = RandomTestData.newRandomTestProduct(
-            user_owner,
-            RandomTestData.newRandomTestAddress()
-        );
-        productEntity.setId(1L);
-        Timestamp[] timestamps = RandomTestData.new2SuccessiveTimestamps();
 
-        when(userService.findByUsername(
-                user_wannabe_borrower.getUsername()))
-                .thenReturn(user_wannabe_borrower);
-        when(userService.findByUsername(
-                user_owner.getUsername()))
-                .thenReturn(user_owner);
-        when(productService.getById(
-                productEntity.getId()))
-                .thenReturn(productEntity);
+        UserEntity user_owner = RandomTestData.newRandomTestUser();
+        userService.addUser(user_owner);
+
+        UserEntity user_wannabe_borrower = RandomTestData.newRandomTestUser();
+        userService.addUser(user_wannabe_borrower);
+
+        ProductEntity productEntity = RandomTestData.newRandomTestProduct(
+                user_owner,
+                RandomTestData.newRandomTestAddress()
+        );
+        productService.addProduct(productEntity);
+
+        Timestamp[] timestamps = RandomTestData.new2SuccessiveTimestamps();
 
         proPayService.changeUserBalanceBy(user_wannabe_borrower.getUsername(),100000);
 
         //user2 wants to lend
         lendingService.requestLending(
-            user_wannabe_borrower,
-            productEntity,
-            timestamps[0],
-            timestamps[1]
+                user_wannabe_borrower,
+                productEntity,
+                timestamps[0],
+                timestamps[1]
         );
 
 
         //lending request accepted
         lendingService.acceptLendingRequest(
-            lendingService.getAllRequestsForUser(user_owner).get(0)
+                lendingService.getAllRequestsForUser(user_owner).get(0)
         );
 
 
         //user2 should see the products he is currently lending
         mockMvc.perform(get(MyLendingsController.url)
-            .with(user(authenticatedUserService.loadUserByUsername(
-                user_wannabe_borrower.getUsername()
-            )))
+                .with(user(authenticatedUserService.loadUserByUsername(
+                        user_wannabe_borrower.getUsername()
+                )))
         )
-            .andExpect(content()
-                .string(containsString(
-                    productEntity.getTitle()
-                ))
+                .andExpect(content()
+                        .string(containsString(
+                                productEntity.getTitle()
+                        ))
             );
     }
 }
--- src/test/java/de/hhu/abschlussprojektverleihplattform/controllers/lending/RequestALendingControllerTest.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/controllers/lending/RequestALendingControllerTest.java
@@ -9,18 +9,15 @@ import de.hhu.abschlussprojektverleihplattform.service.propay.ProPayService;
 import de.hhu.abschlussprojektverleihplattform.utils.RandomTestData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -32,10 +29,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class RequestALendingControllerTest {
 
-    @MockBean
+    @Autowired
     UserService userService;
 
-    @MockBean
+    @Autowired
     ProductService productService;
 
     @Autowired
@@ -50,31 +47,19 @@ public class RequestALendingControllerTest {
     @Test
     public void lendingRequestTest() throws Exception {
         UserEntity user_wannabe_borrower= RandomTestData.newRandomTestUser();
-        //      userService.addUser(user_wannabe_borrower);
-        user_wannabe_borrower.setUserId(1L);
+        userService.addUser(user_wannabe_borrower);
 
         proPayService.changeUserBalanceBy(user_wannabe_borrower.getUsername(),99999);
 
         UserEntity user_owner= RandomTestData.newRandomTestUser();
-        //      userService.addUser(user_owner);
-        user_owner.setUserId(2L);
+        userService.addUser(user_owner);
         ProductEntity productEntity =
                 RandomTestData
                         .newRandomTestProduct(user_owner,RandomTestData.newRandomTestAddress());
-        //      productService.addProduct(productEntity);
-        productEntity.setId(1L);
-
-        when(userService.findByUsername(
-                user_wannabe_borrower.getUsername()))
-                .thenReturn(user_wannabe_borrower);
-        when(userService.findByUsername(
-                user_owner.getUsername()))
-                .thenReturn(user_owner);
-        when(productService.getById(
-                productEntity.getId()))
-                .thenReturn(productEntity);
+        productService.addProduct(productEntity);
 
         System.out.println(productEntity.toString());
+
 
         mockMvc.perform(
                 post(
