f016e6fd9bf07a9e26a770e324ea08df73eed216
Johannes <j.fusenig@gmx.net>
2019-02-26 10:50:38 +0100

    WIP SellService 1

--- src/main/java/de/hhu/abschlussprojektverleihplattform/service/SellService.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/service/SellService.java
@@ -1,5 +1,8 @@
 package de.hhu.abschlussprojektverleihplattform.service;
 
+import de.hhu.abschlussprojektverleihplattform.model.ProductEntity;
+import de.hhu.abschlussprojektverleihplattform.model.Productstatus;
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
 import de.hhu.abschlussprojektverleihplattform.repository.IProductRepository;
 import de.hhu.abschlussprojektverleihplattform.service.propay.IPaymentService;
 import org.springframework.stereotype.Service;
@@ -15,4 +18,13 @@ public class SellService implements ISellService {
         this.paymentService = paymentService;
     }
 
+    public void buyProduct(UserEntity actingUser, ProductEntity product) throws Exception {
+        if(product.getStatus().equals(Productstatus.forLending)){
+            throw new Exception("This Product can only be lend, not bought.");
+        }
+        if(product.getStatus().equals(Productstatus.sold)){
+            throw new Exception("This Product already has been sold.");
+        }
+        //continue here
+    }
 }
