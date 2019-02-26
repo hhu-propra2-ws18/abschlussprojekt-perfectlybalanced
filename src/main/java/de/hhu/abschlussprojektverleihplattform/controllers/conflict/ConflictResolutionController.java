8dedfd9aeb289acf9620b6aa2fd0ce170d995ce8
BatkiThom <thomas@bartkowiaks.de>
2019-02-26 15:22:10 +0100

    erster versuch des PostMappings

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/conflict/ConflictResolutionController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/conflict/ConflictResolutionController.java
@@ -2,6 +2,8 @@
 package de.hhu.abschlussprojektverleihplattform.controllers.conflict;
 
 import de.hhu.abschlussprojektverleihplattform.model.LendingEntity;
+import de.hhu.abschlussprojektverleihplattform.model.Lendingstatus;
+import de.hhu.abschlussprojektverleihplattform.model.ProductEntity;
 import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
 import de.hhu.abschlussprojektverleihplattform.service.LendingService;
 import de.hhu.abschlussprojektverleihplattform.service.ProductService;
@@ -9,10 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -48,5 +53,21 @@ public class ConflictResolutionController {
             return "conflictdetail";
         }
         return "redirekt:/";
+    }
+
+    @PostMapping("/conflictdetail/{id}")
+    public String postResolveConflict (
+            @ModelAttribute("lending") @Valid LendingEntity lendingEntity,
+            @PathVariable Long id
+            ) throws Exception {
+        if (id == lendingEntity.getBorrower().getUserId()) {
+            lendingService.borrowerReceivesSuretyAfterConflict(lendingEntity);
+            lendingEntity.setStatus(Lendingstatus.done);
+        }
+        else {
+            lendingService.ownerReceivesSuretyAfterConflict(lendingEntity);
+            lendingEntity.setStatus(Lendingstatus.done);
+        }
+        return "redirect:/";
     }
 }
