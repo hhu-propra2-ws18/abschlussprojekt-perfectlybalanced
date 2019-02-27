cdd49c8756b918b9b217286bed976becc3a4b6c1
pointbazaar <alex23667@gmail.com>
2019-02-27 11:20:53 +0100

    story test WIP

--- src/test/java/de/hhu/abschlussprojektverleihplattform/storytests/MemfredBesitztEinFahrrad.java
+++ src/test/java/de/hhu/abschlussprojektverleihplattform/storytests/MemfredBesitztEinFahrrad.java
@@ -1,4 +1,50 @@
 package de.hhu.abschlussprojektverleihplattform.storytests;
 
+import de.hhu.abschlussprojektverleihplattform.model.ProductEntity;
+import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import de.hhu.abschlussprojektverleihplattform.service.LendingService;
+import de.hhu.abschlussprojektverleihplattform.service.ProductService;
+import de.hhu.abschlussprojektverleihplattform.service.UserService;
+import de.hhu.abschlussprojektverleihplattform.service.propay.ProPayService;
+import de.hhu.abschlussprojektverleihplattform.utils.RandomTestData;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Timestamp;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MemfredBesitztEinFahrrad {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ProPayService proPayService;
+
+    @Autowired
+    LendingService lendingService;
+
+    @Autowired
+    ProductService productService;
+
+    @Test
+    public void test() throws Exception{
+
+        UserEntity memfred = RandomTestData.newRandomTestUser();
+        memfred.setFirstname("memfred");
+
+        userService.addUser(memfred);
+
+        ProductEntity fahrrad = RandomTestData
+                .newRandomTestProduct(memfred,RandomTestData.newRandomTestAddress());
+
+        productService.addProduct(fahrrad);
+
+        //TODO
+    }
 }
