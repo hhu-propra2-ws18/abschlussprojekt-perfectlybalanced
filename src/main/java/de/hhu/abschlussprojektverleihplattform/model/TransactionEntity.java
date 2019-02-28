98216036c6ec31a8af17920763f52d74250087c1
Huyen Chantal Nguyen <chantalnguyen@hotmail.de>
2019-02-28 11:07:12 +0100

    add transactionEntity

---
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/model/TransactionEntity.java
@@ -0,0 +1,40 @@
+package de.hhu.abschlussprojektverleihplattform.model;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+public class TransactionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long sender;
+    private Long receiver;
+    private int cost;
+    @SuppressFBWarnings(justification="generated code")
+    private Timestamp date;
+
+    public TransactionEntity(){
+
+    }
+
+    public TransactionEntity(
+            Long sender,
+            Long receiver,
+            int cost,
+            Timestamp date
+    ){
+        this.sender = sender;
+        this.receiver = receiver;
+        this.cost = cost;
+        this.date = date;
+    }
+}
