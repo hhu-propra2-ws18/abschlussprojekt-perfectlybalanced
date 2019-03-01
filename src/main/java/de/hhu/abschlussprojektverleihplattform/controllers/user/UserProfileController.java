d5f66ae996e0872c9bdf33844980f71b460ce9f0
BatkiThom <thomas@bartkowiaks.de>
2019-03-01 10:20:07 +0100

    added history in profile view

--- src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/user/UserProfileController.java
+++ src/main/java/de/hhu/abschlussprojektverleihplattform/controllers/user/UserProfileController.java
@@ -1,6 +1,8 @@
 package de.hhu.abschlussprojektverleihplattform.controllers.user;
 
+import de.hhu.abschlussprojektverleihplattform.model.TransactionEntity;
 import de.hhu.abschlussprojektverleihplattform.model.UserEntity;
+import de.hhu.abschlussprojektverleihplattform.service.propay.TransactionService;
 import de.hhu.abschlussprojektverleihplattform.service.propay.adapter.ProPayAdapter;
 import de.hhu.abschlussprojektverleihplattform.service.propay.ProPayService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserProfileController {
@@ -19,11 +23,18 @@ public class UserProfileController {
     @Autowired
     ProPayAdapter proPayAdapter;
 
+    @Autowired
+    TransactionService transactionService;
+
     @GetMapping("/profile")
     public String getProfile(Model model, Authentication auth) throws Exception{
         UserEntity user = (UserEntity) auth.getPrincipal();
+
+        List<TransactionEntity> transactions = transactionService.getAllTransactionsFromUser(user.getUserId());
+
         model.addAttribute("user", user);
         model.addAttribute("user_balance",proPayService.usersCurrentBalance(user.getUsername()));
+        model.addAttribute("transaction", transactions);
 
         return "profile";
     }
